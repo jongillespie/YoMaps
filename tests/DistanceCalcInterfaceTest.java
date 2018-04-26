7e43922cb4daa3c23c1faca70f98dfadb64a2183
Jonathan Gillespie <JG@XJ-B3-TE.local>
2018-04-26 23:18:49 +0100

    Distance Interface for LAT LON Calculation created, Test created but not complete or tested.

---
+++ tests/DistanceCalcInterfaceTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DistanceCalcInterfaceTest {
+
+    public DistanceCalcInterface distanceCalcInterface;
+
+    double lat1 = 52.2589817;
+    double lon1 = -7.1081033;
+
+    double lat2 = 52.2569611;
+    double lon2 = -7.1068123;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void distance() {
+        //double distance = distance(lat1, lat2, lon1, lon2);
+
+    }
+
+}
